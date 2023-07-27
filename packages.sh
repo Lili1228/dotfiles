d18c9412c20a86537669f1c1e46300ef5ba880df
Lili Kurek <github@gramywpsl.pl>
2023-07-27 13:31:15 +0200

    Update packages.sh
    Fix the gammastep package name on Arch

--- packages.sh
+++ packages.sh
@@ -10,8 +10,8 @@ case "$ID" in
 		$_SUDO apk add feh gammastep mpv neovim py3-psutil py3-xlib qtile
 		;;
 	arch|artix)
-		#           wallpaper, night light, media player, editor,  notifications,    CPU usage, screen detection
-    	$_SUDO pacman -Syu feh gammastep-indicator mpv neovim-qt python-dbus-next python-psutil python-xlib qtile
+		# wallpaper, night light, media player, editor,  notifications,    CPU usage, screen detection
+    	$_SUDO pacman -Syu feh gammastep mpv neovim-qt python-dbus-next python-psutil python-xlib qtile
 		;;
 	debian|devuan|linuxmint|ubuntu)
 		# tested on Devuan 4.0 (Debian 11) and Mint 21 Xfce (Ubuntu 22.04)
