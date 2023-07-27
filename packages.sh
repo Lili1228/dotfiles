9ca5062be1de38fe99b61f79299046fdead40e25
Lili Kurek <github@gramywpsl.pl>
2023-07-27 13:41:35 +0000

    Add htop to the list

--- packages.sh
+++ packages.sh
@@ -7,15 +7,15 @@ fi
 . /etc/os-release
 case "$ID" in
 	alpine)
-		$_SUDO apk add feh gammastep mpv neovim py3-psutil py3-xlib qtile
+		$_SUDO apk add feh gammastep htop mpv neovim py3-psutil py3-xlib qtile
 		;;
 	arch|artix)
-		# wallpaper, night light, media player, editor,  notifications,    CPU usage, screen detection
-    	$_SUDO pacman -Syu feh gammastep mpv neovim-qt python-dbus-next python-psutil python-xlib qtile
+		# wallpaper, night light, media player, editor, notifications, CPU usage, screen detection
+    	$_SUDO pacman -Syu feh gammastep htop mpv neovim-qt python-dbus-next python-psutil python-xlib qtile
 		;;
 	debian|devuan|linuxmint|ubuntu)
 		# tested on Devuan 4.0 (Debian 11) and Mint 21 Xfce (Ubuntu 22.04)
-		$_SUDO apt update && $_SUDO apt install feh gammastep mpv neovim-qt python3-cairocffi python3-pip python3-psutil python3-xlib
+		$_SUDO apt update && $_SUDO apt install feh gammastep htop mpv neovim-qt python3-cairocffi python3-pip python3-psutil python3-xlib
 		_DEBVER="$(cat /etc/debian_version)"
 		if [ "$_DEBVER" != "${_DEBVER#11}" ]; then
 			# python3-xcffib is too old for later versions
@@ -30,7 +30,7 @@ case "$ID" in
 		_DEBVER=
 		;;
 	fedora)
-		$_SUDO dnf install feh gammastep-indicator mpv neovim-qt python3-cairocffi python3-dbus-next python3-pip python3-psutil python3-xlib
+		$_SUDO dnf install feh gammastep-indicator htop mpv neovim-qt python3-cairocffi python3-dbus-next python3-pip python3-psutil python3-xlib
 		$_SUDO pip3 install qtile
 		;;
 	*)
