5741b5022421e12159628a957c0618858d638011
Lili1228 <github@gramywpsl.pl>
2023-10-03 15:40:41 +0200

    Alacritty

--- packages.sh
+++ packages.sh
@@ -7,11 +7,11 @@ fi
 . /etc/os-release
 case "$ID" in
 	alpine)
-		$_SUDO apk add feh gammastep htop mpv neovim py3-psutil py3-xlib qtile
+		$_SUDO apk add alacritty feh gammastep htop mpv neovim py3-psutil py3-xlib qtile
 		;;
 	arch|artix)
-		# wallpaper, night light, media player, editor, notifications, CPU usage, screen detection
-    	$_SUDO pacman -Syu feh gammastep htop mpv neovim-qt python-dbus-next python-psutil python-xlib qtile
+		# terminal, wallpaper, night light, media player, editor, notifications, CPU usage, screen detection
+    	$_SUDO pacman -Syu alacritty feh gammastep htop mpv neovim-qt python-dbus-next python-psutil python-xlib qtile
 		;;
 	debian|devuan|linuxmint|ubuntu)
 		# tested on Devuan 4.0 (Debian 11) and Mint 21 Xfce (Ubuntu 22.04)
@@ -21,16 +21,16 @@ case "$ID" in
 			# python3-xcffib is too old for later versions
 			$_SUDO pip3 install qtile==0.18.0
 		elif [ "$VERSION_CODENAME" = jammy ] || [ "$UBUNTU_CODENAME" = jammy ]; then
-			# doesn't have python3-dbus-next in repo
+			# doesn't have alacritty and python3-dbus-next in repo
 			$_SUDO pip3 install dbus-next qtile
 		else
-			$_SUDO apt install python3-dbus-next
+			$_SUDO apt install alacritty python3-dbus-next
 			$_SUDO pip3 install qtile
 		fi
 		_DEBVER=
 		;;
 	fedora)
-		$_SUDO dnf install feh gammastep-indicator htop mpv neovim-qt python3-cairocffi python3-dbus-next python3-pip python3-psutil python3-xlib
+		$_SUDO dnf install alacritty feh gammastep-indicator htop mpv neovim-qt python3-cairocffi python3-dbus-next python3-pip python3-psutil python3-xlib
 		$_SUDO pip3 install qtile
 		;;
 	*)
